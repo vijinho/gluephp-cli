0f8b429e4263c3324b7d2e3c01a9b037aa3d7254
Vijay Mahrra <vijay.mahrra@gmail.com>
2013-12-08 13:55:10 +0100

    reformatted code.  added request method and path as parameters.  aded new method CLI for when run on the command line

--- glue.php
+++ glue.php
@@ -1,75 +1,100 @@
 <?php
 
+/**
+ * glue
+ *
+ * Provides an easy way to map URLs to classes. URLs can be literal
+ * strings or regular expressions.
+ *
+ * When the URLs are processed:
+ *      * delimiter (/) are automatically escaped: (\/)
+ *      * The beginning and end are anchored (^ $)
+ *      * An optional end slash is added (/?)
+ * 	    * The i option is added for case-insensitive searches
+ *
+ * Example:
+ *
+ * $urls = array(
+ *     '/' => 'index',
+ *     '/page/(\d+)' => 'page'
+ * );
+ *
+ * class page {
+ *      function GET($matches) {
+ *          echo "Your requested page " . $matches[1];
+ *      }
+ * }
+ *
+ * glue::stick($urls);
+ *
+ * @author Joe Topjian <joe@topjian.net>
+ * @author Vijay Mahrra <vijay@yoyo.org>
+ * @see http://gluephp.com/
+ * @see https://github.com/jtopjian/gluephp
+ */
+class glue {
+
     /**
-     * glue
+     * stick
      *
-     * Provides an easy way to map URLs to classes. URLs can be literal
-     * strings or regular expressions.
+     * the main static function of the glue class.
      *
-     * When the URLs are processed:
-     *      * delimiter (/) are automatically escaped: (\/)
-     *      * The beginning and end are anchored (^ $)
-     *      * An optional end slash is added (/?)
-     *	    * The i option is added for case-insensitive searches
-     *
-     * Example:
-     *
-     * $urls = array(
-     *     '/' => 'index',
-     *     '/page/(\d+)' => 'page'
-     * );
-     *
-     * class page {
-     *      function GET($matches) {
-     *          echo "Your requested page " . $matches[1];
-     *      }
-     * }
-     *
-     * glue::stick($urls);
-     *
+     * @param   array    	$urls  	    The regex-based url to class mapping
+     * @param   string    	$path  	    The path to the current url $_SERVER['REQUEST_URI']
+     * @param   string    	$method     The http request method, default GET. $_SERVER['REQUEST_METHOD']
+     * @throws  Exception               Thrown if corresponding class is not found
+     * @throws  Exception               Thrown if no match is found
+     * @throws  BadMethodCallException  Thrown if a corresponding GET,POST is not found
      */
-    class glue {
+    static function stick($urls, $path = null, $method = null) {
 
-        /**
-         * stick
-         *
-         * the main static function of the glue class.
-         *
-         * @param   array    	$urls  	    The regex-based url to class mapping
-         * @throws  Exception               Thrown if corresponding class is not found
-         * @throws  Exception               Thrown if no match is found
-         * @throws  BadMethodCallException  Thrown if a corresponding GET,POST is not found
-         *
-         */
-        static function stick ($urls) {
-
-            $method = strtoupper($_SERVER['REQUEST_METHOD']);
-            $path = $_SERVER['REQUEST_URI'];
-
-            $found = false;
-
-            krsort($urls);
-
-            foreach ($urls as $regex => $class) {
-                $regex = str_replace('/', '\/', $regex);
-                $regex = '^' . $regex . '\/?$';
-                if (preg_match("/$regex/i", $path, $matches)) {
-                    $found = true;
-                    if (class_exists($class)) {
-                        $obj = new $class;
-                        if (method_exists($obj, $method)) {
-                            $obj->$method($matches);
-                        } else {
-                            throw new BadMethodCallException("Method, $method, not supported.");
-                        }
-                    } else {
-                        throw new Exception("Class, $class, not found.");
-                    }
+        // check that method is valid
+        $method = strtoupper($method);
+        if (empty($method) && array_key_exists('REQUEST_METHOD', $_SERVER)) {
+            $method = $_SERVER['REQUEST_METHOD'];
+        } else {
+            switch ($method) {
+                case 'POST':
+                case 'PUT':
+                case 'DELETE':
+                case 'CLI':
                     break;
-                }
-            }
-            if (!$found) {
-                throw new Exception("URL, $path, not found.");
+                default:
+                case 'GET':
+                    $method = 'GET';
+                    break;
             }
         }
+
+        if (empty($path) && array_key_exists('REQUEST_URI', $_SERVER)) {
+            $path = $_SERVER['REQUEST_URI'];
+        }
+
+        $found = false;
+
+        krsort($urls);
+
+        foreach ($urls as $regex => $class) {
+            $regex = str_replace('/', '\/', $regex);
+            $regex = '^' . $regex . '\/?$';
+            if (preg_match("/$regex/i", $path, $matches)) {
+                $found = true;
+                if (class_exists($class)) {
+                    $obj = new $class;
+                    if (method_exists($obj, $method)) {
+                        $obj->$method($matches);
+                    } else {
+                        throw new BadMethodCallException("Method, $method, not supported.");
+                    }
+                } else {
+                    throw new Exception("Class, $class, not found.");
+                }
+                break;
+            }
+        }
+        if (!$found) {
+            throw new Exception("URL, $path, not found.");
+        }
     }
+
+}
